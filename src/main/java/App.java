67c267ce7c2301ddd7f2ea513e1f863fb79aaef5
GulakovDmitrii <gulakov127@gmail.com>
2024-07-23 19:26:08 +0300

    V2 вместо equals -> ==

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,10 +10,10 @@ public class App {
         System.out.println(bean.getMessage());
         HelloWorld bean2 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.equals(bean2));
+        System.out.println(bean==(bean2));
 
         Cat bean3 = (Cat) applicationContext.getBean("cat");
         Cat bean4 = (Cat) applicationContext.getBean("cat");
-        System.out.println(bean3.equals(bean4));
+        System.out.println(bean3==(bean4));
     }
 }
